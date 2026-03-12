4d37866ca049afd73409305822beb2d9d941e0c7
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-12 19:38:40 +0200

    Implement SaveConsoleCommand with execute method

--- src/main/java/ex04/SaveConsoleCommand.java
+++ src/main/java/ex04/SaveConsoleCommand.java
@@ -1,4 +1,21 @@
 package ex04;
 
-public class SaveConsoleCommand {
+import ex02.View;
+
+public class SaveConsoleCommand implements ConsoleCommand {
+    private View view;
+
+    public SaveConsoleCommand(View view) {
+        this.view = view;
+    }
+
+    public char getKey() { return 's'; }
+
+    public String toString() { return "'s'ave"; }
+
+    public void execute() throws Exception {
+        System.out.println("  ");
+        view.viewSave();
+        System.out.println("Zberezheno");
+    }
 }
