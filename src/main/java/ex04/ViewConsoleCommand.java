87e5c2cb7bb6b7f7f83d2a7a1053ac9c97187028
glebboxik-cmyk <glebboxik@gmail.com>
2026-03-12 19:39:05 +0200

    Add ViewConsoleCommand class with execute method
    Implement ViewConsoleCommand to execute view actions.

--- src/main/java/ex04/ViewConsoleCommand.java
+++ src/main/java/ex04/ViewConsoleCommand.java
@@ -1,4 +1,20 @@
 package ex04;
 
-public class ViewConsoleCommand {
+import ex02.View;
+
+public class ViewConsoleCommand implements ConsoleCommand {
+    private View view;
+
+    public ViewConsoleCommand(View view) {
+        this.view = view;
+    }
+
+    public char getKey() { return 'v'; }
+
+    public String toString() { return "'v'iew"; }
+
+    public void execute() {
+        System.out.println("  ");
+        view.viewShow();
+    }
 }
